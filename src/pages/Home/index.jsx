5c69e287f9be834436b0348dab6d61c83180bb92
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-21 18:25:38 -0300

    feat: Implementa o componente Header

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import Header from "../../components/Header";
 import styles from "./home.module.css"
 
 export const Home = () => {
     return (
        <div className={styles.homeContainer}>
-            IT IS ALIVEEEEE!!
+            <Header/>
+            <Box>
+
+            </Box>  
+            {/* <Footer/> */}
        </div>
     );
 }
