47f0465acb8eda0391cb78fc8e97264f86a1e826
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-21 19:32:58 -0300

    chore: Adiciona Imagens e Tema do Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import Routes from './routes/RouterProvider'
+import theme from './styles/theme'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Routes />
     </ChakraProvider>
   </StrictMode>,
