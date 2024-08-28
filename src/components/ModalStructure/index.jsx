b53ac34ddf6cd9b5f393e593729ba73dbb85843b
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-28 18:16:46 -0300

    feat: Add Default Modal

---
+++ src/components/ModalStructure/index.jsx
@@ -0,0 +1,42 @@
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react"
+
+export default function ModalStructure(
+    {
+        size = "xl",
+        title = "Modal",
+        isOpen, onClose,
+        contentBody,
+        contentFooter = null
+    }
+    ) {
+
+    return(
+        <Modal
+            size={size}
+            isOpen={isOpen}
+            onClose={onClose}
+        >
+            <ModalOverlay/>
+            <ModalContent borderRadius={"20px"}>
+                <ModalHeader 
+                    textAlign={"center"} 
+                    bg={"main.200"} 
+                    h={"1vw"} 
+                    borderTopRadius={"20px"}
+                    p="0.8vw 0vw 0.4vw 0vw"
+                >
+                    {title}
+                </ModalHeader>
+                <ModalCloseButton/>
+                <ModalBody>
+                    {contentBody}
+                </ModalBody>
+                {contentFooter ? (
+                    <ModalFooter>
+                        {contentFooter}
+                    </ModalFooter>
+                ) : <></>}
+            </ModalContent>
+        </Modal>
+    )
+}
