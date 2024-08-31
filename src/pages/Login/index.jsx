1ba390e050eda88ba37e50d4506d05eab67e974a
Guilherme Alves <drdrakino@gmail.com>
2024-08-31 20:05:20 -0300

    fix: Consertar Viewport dos forms

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,14 @@
-import { Flex } from "@chakra-ui/react";
-import styles from "./login.module.css"
+import styles from "./login.module.css";
+import { Box } from "@chakra-ui/react";
 import Header from "../../components/Header";
 import LoginForm from "../../components/LoginForm";
 
 const Login = () => {
     return (
-        <>
+        <Box className={styles.noflow}>
             <Header />
             <LoginForm />
-        </>
+        </Box>
     );
 }
 
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,14 +1,14 @@
-import { Flex } from "@chakra-ui/react";
 import styles from "./register.module.css"
 import RegisterForm from "../../components/RegisterForm";
 import Header from "../../components/Header";
+import { Box } from "@chakra-ui/react";
 
 const Register = () => {
     return (
-       <>
+       <Box className={styles.noflow}>
         <Header />
         <RegisterForm />
-       </>
+       </Box>
     );
 }
 
