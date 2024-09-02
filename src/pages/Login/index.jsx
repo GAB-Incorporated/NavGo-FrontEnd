2157dd3335bebda023a7d831a07647c1c05e52d3
Guilherme Alves <drdrakino@gmail.com>
2024-09-02 11:55:41 -0300

    feat: Criar primeiro Footer

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,12 +2,14 @@ import styles from "./login.module.css";
 import { Box } from "@chakra-ui/react";
 import Header from "../../components/Header";
 import LoginForm from "../../components/LoginForm";
+import Footer from "../../components/Footer";
 
 const Login = () => {
     return (
         <Box className={styles.noflow}>
             <Header />
             <LoginForm />
+            <Footer />
         </Box>
     );
 }
