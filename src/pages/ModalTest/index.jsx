f4421b544d21931c10d31c7c5d98e936abf59692
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-07 17:23:18 -0300

    feat: Implement Create Course Modal

--- src/pages/ModalTest/index.jsx
+++ src/pages/ModalTest/index.jsx
@@ -1,6 +1,6 @@
 import { Button, useDisclosure } from "@chakra-ui/react";
 import ModalStructure from "../../components/ModalStructure";
-import CourseBody from "../../components/ModalBody/CourseBody"
+import CreateCourse from "../../components/ModalBody/CourseBody/CreateCourse" 
 
 export default function ModalTest() {
     const {isOpen, onOpen, onClose } = useDisclosure()
@@ -13,11 +13,11 @@ export default function ModalTest() {
                 Abre o Modal
             </Button>
             <ModalStructure
-                title="Modal de Teste" 
+                title="Modal de Cursos" 
                 isOpen={isOpen}
                 onOpen={onOpen}
                 onClose={onClose}
-                contentBody={<CourseBody/>}
+                contentBody={<CreateCourse/>}
             />
         </>
     )
