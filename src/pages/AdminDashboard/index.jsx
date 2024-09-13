6fa0f0d3d5949e6ee884a7533a3f62e67939ed4e
Guilherme Alves <drdrakino@gmail.com>
2024-09-12 22:28:20 -0300

    feat: Criar base de estilo do painel administrativo

--- src/pages/AdminDashboard/index.jsx
+++ src/pages/AdminDashboard/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Box, Button, SimpleGrid, Text, useDisclosure } from "@chakra-ui/react";
+import { Box, Button, SimpleGrid, Text, Divider, AbsoluteCenter,useDisclosure } from "@chakra-ui/react";
 import styles from "./adminDashboard.module.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
@@ -8,6 +8,7 @@ import CreateSubjectBody from '../../components/ModalBody/SubjectBody/CreateSubj
 import CourseBody from "../../components/ModalBody/CourseBody";
 import CreateLocationBody from '../../components/ModalBody/LocationBody/CreateLocation'
 import UpdateSubject from '../../components/ModalBody/SubjectBody/UpdateSubject'
+import { DeleteIcon } from "@chakra-ui/icons";
 
   const AdminDashboard = () => {
       const { isOpen, onOpen, onClose } = useDisclosure();
@@ -17,7 +18,7 @@ import UpdateSubject from '../../components/ModalBody/SubjectBody/UpdateSubject'
           setModalContent(content);
           onOpen();
       };
-  
+      
       return (
           <Box className={styles.content}>
               <Header />
@@ -28,55 +29,155 @@ import UpdateSubject from '../../components/ModalBody/SubjectBody/UpdateSubject'
                   title="ADICIONAR NOVO ITEM"
                   contentBody={modalContent}
               />
-
-              <SimpleGrid columns={[1, 2, 3]} spacing={10} mt={8} mb={20}>
+                <Text as="h2">PAINEL ADMINISTRATIVO</Text>
+                <Box position='relative' padding='10'>
+                    <Divider/>
+                    <AbsoluteCenter fontSize='1.5em' bg='white'>
+                        Controle os dados da sua Instituição 
+                    </AbsoluteCenter>
+                </Box>
+                <SimpleGrid columns={[1, 2, 3]} spacing={8} mt={8} mb={20} ml={5} mr={5}>
                   <Box p={5} shadow="md" borderWidth="1px">
-                      <Text fontSize="xl">Matérias</Text>
+                      <Text fontSize="xl" as="h3">Matérias</Text>
+                      
+                      {/* TESTE DE LISTAGEM  */}
+                      <Box as="article">
+                        <Box as="section">
+                            <Text as="p">Enginnering 101 <DeleteIcon _hover={{color: 'black', cursor: 'pointer'}} mb={1}/></Text>
+                        </Box>
+                        <Box as="section">
+                            <Text as="p">Biochemistry 101 <DeleteIcon _hover={{color: 'black', cursor: 'pointer'}} mb={1}/></Text>
+                        </Box>
+                        <Box as="section">
+                            <Text as="p">Capoeira Avançada <DeleteIcon _hover={{color: 'black', cursor: 'pointer'}} mb={1}/></Text>
+                        </Box>
+                      </Box>
+                      <Box as="article">
+                        <Box as="section">
+                            <Text as="p">Enginnering 101 <DeleteIcon _hover={{color: 'black', cursor: 'pointer'}} mb={1}/></Text>
+                        </Box>
+                        <Box as="section">
+                            <Text as="p">Biochemistry 202 <DeleteIcon _hover={{color: 'black', cursor: 'pointer'}} mb={1}/></Text>
+                        </Box>
+                        <Box as="section">
+                            <Text as="p">Capoeira Avançada <DeleteIcon _hover={{color: 'black', cursor: 'pointer'}} mb={1}/></Text>
+                        </Box>
+                      </Box>
+                      <Box as="article">
+                        <Box as="section">
+                            <Text as="p">Enginnering 101 <DeleteIcon _hover={{color: 'black', cursor: 'pointer'}} mb={1}/></Text>
+                        </Box>
+                        <Box as="section">
+                            <Text as="p">Biochemistry 203 <DeleteIcon _hover={{color: 'black', cursor: 'pointer'}} mb={1}/></Text>
+                        </Box>
+                        <Box as="section">
+                            <Text as="p">Capoeira Avançada <DeleteIcon _hover={{color: 'black', cursor: 'pointer'}} mb={1}/></Text>
+                        </Box>
+                      </Box>
+
                       <Button
                           onClick={() => openModal(<CreateSubjectBody />)}
-                          mt={4}
+                          mt={10}
+                          display={'block'}
                           bg={'main.200'}
+                          border={'1px solid #000'}
                       >
                           Adicionar Matéria
                       </Button>
                   </Box>
   
                   <Box p={5} shadow="md" borderWidth="1px">
-                      <Text fontSize="xl">Locais</Text>
+                      <Text fontSize="xl" as="h3">Locais</Text>
                       <Button
-                          onClick={() => openModal(<CreateLocationBody />)}  // Implementação semelhante para Professores
+                          onClick={() => openModal(<CreateLocationBody />)} 
                           mt={4}
                           bg={'main.200'}
+                          border={'1px solid #000'}
                       >
                           Adicionar Local
                       </Button>
                   </Box>
   
                   <Box p={5} shadow="md" borderWidth="1px">
-                      <Text fontSize="xl">Prédios</Text>
+                      <Text fontSize="xl" as="h3">Prédios</Text>
                       <Button
-                          onClick={() => openModal(<UpdateSubject/>)}  // Implementação semelhante para Alunos
+                          onClick={() => openModal(<UpdateSubject/>)}  
                           mt={4}
                           bg={'main.200'}
+                          border={'1px solid #000'}
                       >
                           Adicionar Prédio
                       </Button>
                   </Box>
 
                   <Box p={5} shadow="md" borderWidth="1px">
-                      <Text fontSize="xl">Cursos</Text>
+                      <Text fontSize="xl" as="h3">Cursos</Text>
                       <Button
-                          onClick={() => openModal(<CourseBody />)}  // Implementação semelhante para Alunos
+                          onClick={() => openModal(<CourseBody />)}  
                           mt={4}
                           bg={'main.200'}
+                          border={'1px solid #000'}
                       >
                           Adicionar Curso
                       </Button>
                   </Box>
-
-              </SimpleGrid>
-  
-              <Footer />
+                  <Box p={5} shadow="md" borderWidth="1px">
+                      <Text fontSize="xl" as="h3">Cursos</Text>
+                      <Button
+                          onClick={() => openModal(<CourseBody />)}  
+                          mt={4}
+                          bg={'main.200'}
+                          border={'1px solid #000'}
+                      >
+                          Adicionar Curso
+                      </Button>
+                  </Box>
+                  <Box p={5} shadow="md" borderWidth="1px">
+                      <Text fontSize="xl" as="h3">Cursos</Text>
+                      <Button
+                          onClick={() => openModal(<CourseBody />)}  
+                          mt={4}
+                          bg={'main.200'}
+                          border={'1px solid #000'}
+                      >
+                          Adicionar Curso
+                      </Button>
+                  </Box>
+                  <Box p={5} shadow="md" borderWidth="1px">
+                      <Text fontSize="xl" as="h3">Cursos</Text>
+                      <Button
+                          onClick={() => openModal(<CourseBody />)}  
+                          mt={4}
+                          bg={'main.200'}
+                          border={'1px solid #000'}
+                      >
+                          Adicionar Curso
+                      </Button>
+                  </Box>
+                  <Box p={5} shadow="md" borderWidth="1px">
+                      <Text fontSize="xl" as="h3">Cursos</Text>
+                      <Button
+                          onClick={() => openModal(<CourseBody />)}  
+                          mt={4}
+                          bg={'main.200'}
+                          border={'1px solid #000'}
+                      >
+                          Adicionar Curso
+                      </Button>
+                  </Box>
+                  <Box p={5} shadow="md" borderWidth="1px">
+                      <Text fontSize="xl" as="h3">Cursos</Text>
+                      <Button
+                          onClick={() => openModal(<CourseBody />)}  
+                          mt={4}
+                          bg={'main.200'}
+                          border={'1px solid #000'}
+                      >
+                          Adicionar Curso
+                      </Button>
+                  </Box>
+                </SimpleGrid>
+                <Footer />
           </Box>
       );
   };
