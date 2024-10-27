77141c7b2a4e31b121a7e2845d0ee08705f924cb
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-10-27 17:26:42 -0300

    feat: Implementa Modais de Tutoriais

---
+++ src/components/ModalBody/SubhomeBody/MuralBody/index.jsx
@@ -0,0 +1,27 @@
+import { Box, Text } from "@chakra-ui/react"
+
+const MuralBody = () => {
+
+    return (
+        <Box>
+            <Box bgColor={'main.300'} m="1vw" pl="0.5vw" pr="0.5vw" borderRadius='20px'>
+                <Text>
+                    No mural será possivel visualizar os arquivos enviados 
+                    por professores referentes a diferentes aulas.
+                </Text>
+                <Text>
+                    Para essa funcionalidade funcionar corretamente, atualmente 
+                    é necessário você ter um login na ferramenta, justamente para 
+                    vincularmos seu usuário ao mural do seu respectivo curso.
+                </Text>
+                <Text>
+                    Posteriormente, na barra lateral você poderá selecionar seu curso, 
+                    a materia do mesmo e visualizar os materiais enviados pelo professor da matéria.
+                </Text>
+                <Image src="imagemManeira.jpg" aria-label="Imagem da barra lateral, mostrando os murais de cada materia."/>
+            </Box>
+        </Box>
+    )
+}
+
+export default MuralBody;
