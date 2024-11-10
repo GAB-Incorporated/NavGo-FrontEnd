4191bd546467c4dbe089b6cf08699b6471995bf9
Guilherme Alves <drdrakino@gmail.com>
2024-11-10 19:17:26 -0300

    feat: Renderizar nós

--- src/components/InternalMap/index.jsx
+++ src/components/InternalMap/index.jsx
@@ -3,37 +3,51 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-fullscreen';
 import 'leaflet.fullscreen/Control.FullScreen.css';
-import api from '../../api.js'
+import api from '../../api.js';
 
 const InternalMap = () => {
   const [locations, setLocations] = useState([]);
+  const [path, setPath] = useState([]);
 
   useEffect(() => {
-    const fetchInternalLocations = async () => {
+    const fetchData = async () => {
       try {
-        const response = await api.get('/locations');
-        setLocations(response.data);
+        const locationsResponse = await api.get('/locations');
+        setLocations(locationsResponse.data);
+
       } catch (err) {
         console.error(err);
       }
     };
 
-    fetchInternalLocations();
+    fetchData();
   }, []);
+
+  const calculateRoute = async () => {
+    const start = { x: 72.00, y: 8.00, floor: 0, building_id: 1 };
+    const end = { x: 20.00, y: 20.00, floor: 0, building_id: 1 };
+
+    try {
+      const response = await api.post('/routing/route', {
+        start,
+        end,
+      });
+      setPath(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
     let map, sidebar;
 
     if (!map) {
-    
-        // Estilos das paredes
         const wallStyle = {
             color: 'black',
             weight: 2,
             opacity: 1,
         };
 
-        // Cria o mapa interno
         map = L.map('internalMap', {
             crs: L.CRS.Simple,
             minZoom: 0,
@@ -50,9 +64,7 @@ const InternalMap = () => {
 
         const layers = {};
 
-        // Mapeia as locations dinamicamente
         locations.forEach((location) => {
-
         const layer = L.polygon(location.coordinates, wallStyle).on('click', function () {
           sidebar.show();
         });
@@ -72,22 +84,29 @@ const InternalMap = () => {
         };
 
         L.control.layers(baseMaps, null, { collapsed: false }).addTo(map);
+
+        // Desenha os nodes como bolinhas
+        if (path.length > 0) {
+            path.forEach(node => {
+                L.circle([node.x, node.y], { radius: 0.5, color: 'blue' }).addTo(map);
+            });
+        }
     }
 
-    // Remove o externo para renderizar o interno
     return () => {
         if (map) {
             map.remove();
             map = null;
         }
     };
-  }, [locations]);
+  }, [locations, path]);
 
-    return (
-        <div style={{height: "100vh", width: "100%"}}>
-            <div id="internalMap" style={{height: "100%"}}></div>
-            <div id="sidebar"></div>
-        </div>
+  return (
+      <div style={{height: "100vh", width: "100%"}}>
+          <div id="internalMap" style={{height: "100%"}}></div>
+          <div id="sidebar"></div>
+          <button onClick={calculateRoute}>Calculate Route</button> {/* Botão horrível no fim da tela para teste */}
+      </div>
   );
 };
 
