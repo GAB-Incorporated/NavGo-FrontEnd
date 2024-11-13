179537d7d636afc7b803001c0076db72f5914b02
Aurora Kruschewsky <124530477+AurorinhaBoreal@users.noreply.github.com>
2024-11-13 14:25:53 -0300

    Adm dashboard - Tab Resumo Funcional (#35)
    * fix: Minor Changes in Subhome
    
    * feat: Dashboard Header Component
    
    * feat: Empty Buildings Tab
    
    * feat: Broken Location from Building
    
    * feat: Empty Tabs for Reference
    
    * fix: Rmove Footer on Login and Register
    
    * feat: Implement Summary Tab
    
    * feat: Implement DashboardPanel Component
    
    * chore: Forgot this css File kk
    
    * feat: Implement Dashboard Page
    
    * chore: Implement Static Images for Dashboard
    
    * chore: Remove Old Dashboard
    
    * fix: Remove Footer from Register
    
    * chore: Remove Unnecessary Files
    
    * fix: LINT DESGRAÇADO
    
    * fix: Fix a little Lint
    
    * fix: Arruma endpoint
    
    * fix: Arruma endpoints e adiciona professor
    
    * feat: Modal base para visualização de localidades
    
    ---------
    
    Co-authored-by: Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
    Co-authored-by: Bruno Silva <142853863+brunofsilva2@users.noreply.github.com>

--- src/components/ModalBody/Buildings/index.jsx
+++ src/components/ModalBody/Buildings/index.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import api from "../../../api";
+import styles from './buildings.module.css'
 
-const Buildings = (buildingName) => {
-    const [locations, setLocations] = useState()
+const Buildings = ({buildingID, buildingName}) => {
+    const [locations, setLocations] = useState([])
 
+    console.log(buildingID+buildingName)
     const fetchLocations = async () => {
-        const locationData = await api.get("/location/"+buildingName)
+        const locationData = await api.get("/locations/fromBuilding/"+buildingID)
         setLocations(locationData.data)
     }
 
@@ -16,15 +18,30 @@ const Buildings = (buildingName) => {
 
     return(
         <Box m="2vw">
-            <Text>Localizações da {buildingName}</Text>
+            <Text textAlign="center">Localizações da {buildingName}</Text>
             {locations.map((location) => (
-                <Box key={location.location_name} bg="white" p="2vw" m="1vw">
-                    <Text key={location.campus}>{location.campus}</Text>
-                    <Text key={location.location_name}>{location.location_name}</Text>
-                    <Text key={location.description}>{location.description}</Text>
-                    <Text key={location.open_time}>{location.open_time}</Text>
-                    <Text key={location.closing_time}>{location.closing_time}</Text>
-                </Box>
+                <>
+                    <Box key={location.location_name} bg="white" p="1vw" m="1vw" w="inherit"
+                        borderColor="main.100" borderWidth="10px" borderRadius={30}>
+                        <Text className={styles.info} key={location.campus}>{location.campus}</Text>
+                        <Flex gap="1vw">
+                            <label className={styles.infoLabel}>Nome:</label>
+                            <Text className={styles.info} key={location.location_name}>{location.location_name}</Text>
+                        </Flex>
+                        <Flex gap="1vw">
+                            <label className={styles.infoLabel}>Descrição:</label>
+                            <Text className={styles.info} key={location.description}>{location.description}</Text>
+                        </Flex>
+                        <Flex gap="1vw">
+                            <label className={styles.infoLabel}>Hora Abertura:</label>
+                            <Text className={styles.info} key={location.open_time}>{location.open_time}</Text>
+                        </Flex>
+                        <Flex gap="1vw">
+                            <label className={styles.infoLabel}>Hora Fechamento:</label>
+                            <Text className={styles.info} key={location.closing_time}>{location.closing_time}</Text>
+                        </Flex>
+                    </Box>
+                </>
             ))}
         </Box>
     )
