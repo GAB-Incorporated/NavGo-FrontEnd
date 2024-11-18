b1e4d57701871889db4d5e9190e7e741bef9b65b
Guilherme Alves <drdrakino@gmail.com>
2024-11-18 19:27:17 -0300

    Home fix & public (#38)
    * Consertar alinhamento das imagens
    
    * fix: Conserta imagens implementando pasta public
    
    * fix: Alinha degrade corretamente por cima das imagens
    
    * fix: Retira importações da pasta public
    
    * feat: Responsividade para mobile padrão
    
    * style: Rota do mapa bonita com icone

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,59 +5,56 @@ import styles from "./home.module.css";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
+
     return (
        <Flex className={styles.homeContainer}>
             <Header />
-            <Box className={styles.section}>
-                <Box className={styles.sectionImage1}>
-                    <Box className={styles.sectionBody1}>
-                        <Flex className={styles.contentWrapper}>
-                            <Text className={styles.title}>
-                                MAPA DA INSTITUIÇÃO
-                            </Text>
-                            <Text className={styles.description}>
-                                O Mapa NavGo permite uma identificação precisa e eficiente dos diversos espaços da instituição com a funcionalidade de rotas integrada, cada estudante tem a garantia de encontrar o caminho correto e se orientar facilmente dentro do ambiente acadêmico.
-                            </Text>
-                            <Box className={styles.buttonWrapper}>
-                                <Link to={"/login/map"}>
-                                    <Button className={styles.sectionButton1}>
-                                        Abrir Mapa
-                                    </Button>
-                                </Link>
-                                <Link to={"/login"}>
-                                    <Button className={styles.sectionButton1}>
-                                        Utilização
-                                    </Button>
-                                </Link>
-                            </Box>
-                        </Flex>
-                    </Box>
+            <Box className={styles.sectionImage1}>
+                <Box className={styles.sectionBody1}>
+                    <Flex className={styles.contentWrapper1}>
+                        <Text className={styles.title}>
+                            MAPA DA INSTITUIÇÃO
+                        </Text>
+                        <Text className={styles.description1}>
+                            O Mapa NavGo permite uma identificação precisa e eficiente dos diversos espaços da instituição com a funcionalidade de rotas integrada, cada estudante tem a garantia de encontrar o caminho correto e se orientar facilmente dentro do ambiente acadêmico.
+                        </Text>
+                        <Box className={styles.buttonWrapper1}>
+                            <Link to={"/login/map"}>
+                                <Button className={styles.sectionButton1}>
+                                    Abrir Mapa
+                                </Button>
+                            </Link>
+                            <Link to={"/login"}>
+                                <Button className={styles.sectionButton1}>
+                                    Utilização
+                                </Button>
+                            </Link>
+                        </Box>
+                    </Flex>
                 </Box>  
             </Box>
-            <Box className={styles.section}>
-                <Box className={styles.sectionImage2}>
-                    <Box className={styles.sectionBody2}>
-                        <Flex className={styles.contentWrapper}>
-                            <Text className={styles.title}>
-                                TRANSFERÊNCIA DE ARQUIVOS
-                            </Text>
-                            <Text className={styles.description}>
-                                O Mural NavGo aprimora a experiência acadêmica ao concentrar todos os materiais de aula em um único ambiente digital, de fácil acesso, oferecendo uma plataforma acessível e organizada para todos os estudantes, e elimina a necessidade de utilizar ferramentas externas não especializadas no contexto educacional.
-                            </Text>
-                            <Box className={styles.buttonWrapper}>
-                                <Link to={"/login/mural"}>
-                                    <Button className={styles.sectionButton2}>
-                                        Transferir Arquivos
-                                    </Button>
-                                </Link>
-                                <Link to={"/login"}>
-                                    <Button className={styles.sectionButton2}>
-                                        Utilização
-                                    </Button>
-                                </Link>
-                            </Box>
-                        </Flex>
-                    </Box>
+            <Box className={styles.sectionImage2}>
+                <Box className={styles.sectionBody2}>
+                    <Flex className={styles.contentWrapper2}>
+                        <Text className={styles.title2}>
+                            TRANSFERÊNCIA DE ARQUIVOS
+                        </Text>
+                        <Text className={styles.description2}>
+                            O Mural NavGo aprimora a experiência acadêmica ao concentrar todos os materiais de aula em um único ambiente digital, de fácil acesso, oferecendo uma plataforma acessível e organizada para todos os estudantes, e elimina a necessidade de utilizar ferramentas externas não especializadas no contexto educacional.
+                        </Text>
+                        <Box className={styles.buttonWrapper2}>
+                            <Link to={"/login/mural"}>
+                                <Button className={styles.sectionButton2}>
+                                    Transferir Arquivos
+                                </Button>
+                            </Link>
+                            <Link to={"/login"}>
+                                <Button className={styles.sectionButton2}>
+                                    Utilização
+                                </Button>
+                            </Link>
+                        </Box>
+                    </Flex>
                 </Box>  
             </Box>
             <Footer />
